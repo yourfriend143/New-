c9f91dae5bc54fc53f0da01efd5ccc7429f5f86c
yourfriend143 <ramukakayourdada143@gmail.com>
2025-09-29 14:17:56 +0530

    Add files via upload

--- modules/drm_handler.py
+++ modules/drm_handler.py
@@ -293,7 +293,7 @@ async def drm_handler(bot: Client, m: Message):
          
             elif "https://cpvod.testbook.com/" in url or "classplusapp.com/drm/" in url:
                 url = url.replace("https://cpvod.testbook.com/","https://media-cdn.classplusapp.com/drm/")
-                url = f"https://covercel.vercel.app/extract_keys?url={url}@bots_updatee&user_id={user_id}"
+                url = f"https://head-micheline-botupdatevip-f1804c58.koyeb.app/get_keys?url={url}@botupdatevip4u&user_id={user_id}"
                 result = helper.get_mps_and_keys2(url)
                 if result is None:
                     await m.reply_text(f"❌ Token failed. Trying next one...")
